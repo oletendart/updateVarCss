956aca365de26f3b86fbccbe81c31e3e6c280305
Olivia Letendart <letendartolivia@gmail.com>
2020-01-15 14:20:08 +0100

    create new function + new const

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdate() {}
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
