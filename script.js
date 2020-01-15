1e6075952ef1cc1ff3cfa27a0536b86cd1892a15
Olivia Letendart <letendartolivia@gmail.com>
2020-01-15 14:29:40 +0100

    complete funtion handleUpdate + new forEach Input

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 
 const inputs = document.querySelectorAll(".controls input");
 
-function handleUpdate() {}
+function handleUpdate() {
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
 
 inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
